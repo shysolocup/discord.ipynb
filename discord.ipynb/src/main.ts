2a6e7ffc5c41d0d2bf5cd133282512a79b9c6144
shysolocup <88659700+shysolocup@users.noreply.github.com>
2025-02-20 15:44:13 +0000

    it's running ig

--- discord.ipynb/src/main.ts
+++ discord.ipynb/src/main.ts
@@ -2,12 +2,14 @@ import { dirname, importx } from "@discordx/importer";
 import type { Interaction, Message } from "discord.js";
 import { IntentsBitField } from "discord.js";
 import { Client } from "discordx";
+import fetch from 'node-fetch';
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+// const a = fs.readdirSync(path.join(__dirname, "../../app/users"));
 
 export const bot = new Client({
-  // To use only guild command
-  // botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
-
-  // Discord intents
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
@@ -26,39 +28,14 @@ export const bot = new Client({
   },
 });
 
+
 bot.once("ready", () => {
-  // Make sure all guilds are cached
-  // await bot.guilds.fetch();
-
-  // Synchronize applications commands with Discord
-  void bot.initApplicationCommands();
-
-  // To clear all guild commands, uncomment this line,
-  // This is useful when moving from guild commands to global commands
-  // It must only be executed once
-  //
-  //  await bot.clearApplicationCommands(
-  //    ...bot.guilds.cache.map((g) => g.id)
-  //  );
-
   console.log("Bot started");
 });
 
-bot.on("interactionCreate", (interaction: Interaction) => {
-  bot.executeInteraction(interaction);
-});
-
-bot.on("messageCreate", (message: Message) => {
-  void bot.executeCommand(message);
-});
 
 async function run() {
-  // The following syntax should be used in the commonjs environment
-  //
-  // await importx(__dirname + "/{events,commands}/**/*.{ts,js}");
-
-  // The following syntax should be used in the ECMAScript environment
-  await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
+  await importx(`${dirname(import.meta.url)}/{events}/**/*.{ts,js}`);
 
   // Let's start the bot
   if (!process.env.BOT_TOKEN) {
