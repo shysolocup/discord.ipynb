f02b7dec18fae5e1691d039616cc858b2cfd92aa
shysolocup <88659700+paishee@users.noreply.github.com>
2025-03-05 19:41:15 -0600

    guh

--- discord.ipynb/src/index.ts
+++ discord.ipynb/src/index.ts
@@ -3,6 +3,24 @@ import * as path from 'path';
 
 export const appPath = path.join("app", "./discord.ipynb");
 
+export type Account = {
+  user: {
+    id: string,
+    username: string,
+    token: string,
+    active: boolean
+  },
+
+  settings: {
+
+  }
+}
+
+export const accounts: { [ key : string]: Account } = {};
+import data from '../../app/accounts.json' with { type: "json" };
+Object.assign(accounts, data);
+
+
 export type Notebook = {
   cells: cell.CellConstructor[]
 }
@@ -11,29 +29,15 @@ export function read() {
   return JSON.parse(fs.readFileSync(appPath).toString()) as Notebook;
 }
 
-export function loadPage(pagepath: string) {
+export async function loadPage(pagepath: string) {
   fs.writeFileSync(appPath, fs.readFileSync(path.join("discord.ipynb", `./src/pages/${pagepath}/index.ipynb`)).toString());
+  (await import(`./pages/${pagepath}/index.js`)).init();
 }
 
 import * as cell from './classes/Cell.js'
 import * as image from './classes/Image.js';
 import * as div from './classes/Div.js';
 
-loadPage("start");
-
-let newCell = cell.init({
-  cell_type: cell.cellType.markdown,
-  source: [
-    "a",
-    "b",
-    image.init({
-      src: "https://blog.pinwheel.com/hubfs/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png",
-      size: 50,
-      div: true,
-      align: div.alignment.center
-    }),
-    "c"
-  ]
-})
-
-console.log(newCell);
+(async () => {
+  await loadPage("start");
+})()
---
+++ discord.ipynb/src/pages/start/index.ts
@@ -0,0 +1,6 @@
+import { accounts, read } from '../../index.js';
+
+export async function init() {
+    console.log('a')
+    console.log(accounts);
+}
