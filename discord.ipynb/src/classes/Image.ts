a038a0a1245238b0fb4da0c5a1330ca9e613b9b7
shysolocup <88659700+shysolocup@users.noreply.github.com>
2025-03-07 17:11:25 +0000

    guh

--- discord.ipynb/src/classes/Image.ts
+++ discord.ipynb/src/classes/Image.ts
@@ -4,14 +4,19 @@ export interface Image extends div.Div {
     src : string,
     size? : number,
     alt? : string
+    width? : number,
+    height? : number
 }
 
 export function init (data : Image ) {
     let base = div.init(data);
+
+    data.height = (!data.height && data.size) ? data.size : data.height; 
     
     base += `<img src="${data.src}"`;
 
-    if (data.size) base += ` height=${data.size}`;
+    if (data.size) base += ` height=${data.height}`;
+    if (data.width) base += ` width=${data.width}`;
     if (data.alt) base += ` alt="${data.alt}"`;
 
     return base + ">" + ( (data.div) ? "</div>" : "" )
