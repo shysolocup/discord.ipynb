9ef33424dafce9ce8db7045e492698ce2ad6dbb4
shysolocup <88659700+shysolocup@users.noreply.github.com>
2025-03-10 15:04:46 +0000

    guh

--- discord.ipynb/src/index.ts
+++ discord.ipynb/src/index.ts
@@ -9,10 +9,12 @@ export const client = new Client({
 
 client.once('ready', (ctx) => {
   console.log('ready');
-})
+});
 
+/**
+ * path to the discord.ipynb file
+ */
 export const appPath = path.join("app", "./discord.ipynb");
-
 
 // account layout
 export type Account = {
@@ -25,6 +27,7 @@ export type Account = {
 }
 
 export type SessionData = {
+  userId: string
   place: string
   metadata: {}
 }
@@ -35,6 +38,7 @@ import data from '../../app/accounts.json' with { type: "json" };
 Object.assign(accounts, data);
 
 export const sessionData: SessionData = {
+  userId: "",
   place: "",
   metadata: {}
 }
@@ -43,29 +47,54 @@ export type Notebook = {
   cells: cell.CellConstructor[]
 }
 
+/**
+ * reads from the discord.ipynb file
+ * @returns {Notebook}
+ */
 export function read() {
   return JSON.parse(fs.readFileSync(appPath).toString()) as Notebook;
 }
 
+/**
+ * writes to the discord.ipynb file
+ * @param {Object} filedata - object with data to write to the file
+ * @returns {undefined}
+ */
 export function write(filedata : {}) {
   return fs.writeFileSync(appPath, JSON.stringify(filedata, null, 4));
 }
 
-export async function loadPage(pagepath: string) {
-  let base = path.join("discord.ipynb", `./src/pages/${pagepath}/`);
+/**
+ * loads a page
+ * @param {string} pagename - name of the page to load
+ * @param {boolean} runscript - if it should run an index.ts file in a page
+ * @returns {any | undefined}
+ */
+export async function loadPage(pagename: string, runscript: boolean = true) {
+  let base = path.join("discord.ipynb", `./src/pages/${pagename}/`);
 
   // write to the app's file using the page
   fs.writeFileSync(appPath, fs.readFileSync(base+"index.ipynb").toString());
 
   // if an index.ts file exists in the page's directory run that
-  fs.access(base+'index.ts', async err => !err && (await import(`./pages/${pagepath}/index.js`)).init());
+  try {
+    fs.accessSync(base+'index.ts');
+    return runscript && (await import(`./pages/${pagename}/index.js`)).init();
+  }
+
+  catch {}
 }
+
+
+// imports
 
 import * as cell from './classes/Cell.js'
 import * as image from './classes/Image.js';
 import * as div from './classes/Div.js';
 
+
 // load init page
+
 (async () => {
   await loadPage("start");
 })()
